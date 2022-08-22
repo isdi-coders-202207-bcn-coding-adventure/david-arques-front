758e5a5a01594853d7749a5623cc81c098781083
David Arques <david.darqsa@gmail.com>
2022-08-22 15:44:14 +0200

    Fixed Layout Counter render

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { Counter } from "../../store/features/counter/Counter";
+import Counter from "../Counter/Counter";
 import Header from "../Header/Header";
 
 const StyledContainer = styled.div`
